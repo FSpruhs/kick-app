94726dab60ff52dbc7c17f9d72013125eac82460
spruhs <fabian@spruhs.com>
2025-04-18 20:13:20 +0200

    git workflow

--- backend-kotlin/src/test/kotlin/com/spruhs/kick_app/KickAppApplicationTests.kt
+++ backend-kotlin/src/test/kotlin/com/spruhs/kick_app/KickAppApplicationTests.kt
@@ -1,16 +1,16 @@
-package com.spruhs.kick_app
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
-
-//@Import(TestcontainersConfiguration::class)
-@SpringBootTest
-@ActiveProfiles("test")
-class KickAppApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+//package com.spruhs.kick_app
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//import org.springframework.test.context.ActiveProfiles
+//
+////@Import(TestcontainersConfiguration::class)
+//@SpringBootTest
+//@ActiveProfiles("test")
+//class KickAppApplicationTests {
+//
+//	@Test
+//	fun contextLoads() {
+//	}
+//
+//}

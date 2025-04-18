0dc7a1539bcbf4a76cd502b5e2bd2cee1da2ba2b
spruhs <fabian@spruhs.com>
2025-04-18 19:42:39 +0200

    git workflow

--- backend-kotlin/src/test/kotlin/com/spruhs/kick_app/KickAppApplicationTests.kt
+++ backend-kotlin/src/test/kotlin/com/spruhs/kick_app/KickAppApplicationTests.kt
@@ -1,16 +1,14 @@
 package com.spruhs.kick_app
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 
-@Import(TestcontainersConfiguration::class)
+//@Import(TestcontainersConfiguration::class)
 @SpringBootTest
 class KickAppApplicationTests {
 
 	@Test
-	@Disabled
 	fun contextLoads() {
 	}
 

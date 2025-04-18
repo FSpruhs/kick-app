0ba1f67c921bc9d9dbaeac416715003f28d08229
spruhs <fabian@spruhs.com>
2025-04-18 21:43:17 +0200

    git workflow

--- backend-kotlin/src/test/kotlin/com/spruhs/kick_app/KickAppApplicationTests.kt
+++ backend-kotlin/src/test/kotlin/com/spruhs/kick_app/KickAppApplicationTests.kt
@@ -4,7 +4,7 @@
 //import org.springframework.boot.test.context.SpringBootTest
 //import org.springframework.test.context.ActiveProfiles
 //
-////@Import(TestcontainersConfiguration::class)
+//@Import(TestcontainersConfiguration::class)
 //@SpringBootTest
 //@ActiveProfiles("test")
 //class KickAppApplicationTests {

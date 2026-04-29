56af8cc040b3a934befbc60d6bdb28f8c1f55e42
spruhs <fabian@spruhs.com>
2026-04-29 23:18:56 +0200

    clean up

--- src/test/kotlin/com/spruhs/DatabaseConfig.kt
+++ src/test/kotlin/com/spruhs/DatabaseConfig.kt
@@ -1,10 +1,8 @@
 package com.spruhs
-import com.mongodb.client.MongoClient
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
-import org.springframework.jdbc.core.JdbcTemplate
+
 @Configuration
 @EnableMongoRepositories
 @EntityScan
